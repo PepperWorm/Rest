8542c08066bf54de85c318b997cccce3d0ffaa8e
Sergey Zharkov <jarkov02@mail.ru>
2023-09-14 23:36:37 +0300

    Initial commit

---
+++ src/main/java/ru/kata/spring/boot_security/demo/service/UserServiceImp.java
@@ -0,0 +1,60 @@
+package ru.kata.spring.boot_security.demo.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.kata.spring.boot_security.demo.model.User;
+import ru.kata.spring.boot_security.demo.repository.UserRepository;
+
+
+import java.util.List;
+
+
+@Service
+@Transactional(readOnly = true)
+public class UserServiceImp implements UserService{
+
+    private UserRepository userRepository;
+
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public UserServiceImp(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+
+    @Override
+    public List<User> listUsers() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    @Transactional
+    public User save(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userRepository.save(user);
+    }
+
+    @Override
+    @Transactional
+    public void update(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        userRepository.save(user);
+    }
+
+    @Override
+    @Transactional
+    public void delete(Long id) {
+        userRepository.deleteById(id);
+    }
+
+    @Override
+    public User getById(Long id) {
+        return userRepository.findById(id).get();
+    }
+
+
+}
